5c4b610b1b3a67deb1aabe73af90ef25d725c796
Alexander Zielenski <support@alexzielenski.com>
2022-03-16 08:57:53 -0700

    symlink fzf install script

--- fzf/install.fish
+++ fzf/install.fish
@@ -1,3 +1,6 @@
 #!/usr/bin/env fish
 
 brew install -q fzf
+
+set fzfPath (brew --prefix fzf)
+$fzfPath/install --xdg --completion --key-bindings --no-update-rc
---
+++ scripts/install/fzf.fish
@@ -0,0 +1,1 @@
+../../fzf/install.fish
