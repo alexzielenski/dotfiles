b7173bfd811ed7fd527deba5e83cfb7167c0aefc
Alexander Zielenski <zielenski@google.com>
2022-03-16 16:30:17 -0700

    add hooks for local configuration

---
+++ config.sh
@@ -0,0 +1,5 @@
+# Configuration for the dotfiles bootstrap itself
+# Only exports/aliases should be in here
+# This file is sourced first before brew is configured
+# Use this to customize brew install location if needed
+# export BREW_PREFIX="/usr/local/brew"
---
+++ env.sh
@@ -0,0 +1,3 @@
+# Configuration for the dotfiles bootstrap itself
+# Only exports/aliases should be in here
+# This file is sourced before modules rc files are loaded
