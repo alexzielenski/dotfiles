9e29c9848d9d14957cd7a56a5c5a2a027bc1a22a
Alexander Zielenski <support@alexzielenski.com>
2022-03-17 08:14:25 -0700

    tree aliases

--- colorls/rc_fish.fish
+++ colorls/rc_fish.fish
@@ -1,4 +1,12 @@
 # only alias ls in interactive mode
 if status --is-interactive
     alias ls="colorls"
+
+    function tx
+        colorls --sd --gs --tree=$argv[1] $argv[2..-1]
+    end
+
+    alias t="tx 1"
+    alias t2="tx 2"
+    alias t3="tx 3"
 end
--- colorls/rc_zsh.sh
+++ colorls/rc_zsh.sh
@@ -1,4 +1,12 @@
 # only alias ls in interactive shells
 if [[ $- == *i* ]]; then
     alias ls="colorls"
+
+    function tx {
+        colorls --sd --gs --tree=$1 "${@:2}"
+    }
+
+    alias t="tx 1"
+    alias t2="tx 2"
+    alias t3="tx 3"
 fi
