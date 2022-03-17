401f3135ae1a2f8d61a90a59badcceede145cdbb
Alexander Zielenski <support@alexzielenski.com>
2022-03-16 20:32:59 -0700

    fix zsh z

--- zoxide/rc_zsh.sh
+++ zoxide/rc_zsh.sh
@@ -2,5 +2,5 @@ if command -v brew &> /dev/null
 then
     # Must be called AFTER compinit for working completions
     # As of writing our current zsh config doesnt use completions
-    eval "$(zoxide init posix --hook prompt)"
+    eval "$(zoxide init zsh)"
 fi
