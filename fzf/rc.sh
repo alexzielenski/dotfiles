e98c520e5576bb4d2f6bcc84448b1f410cebfba5
Alexander Zielenski <support@alexzielenski.com>
2022-03-17 19:45:45 -0700

    fzf color scheme

--- fzf/rc.sh
+++ fzf/rc.sh
@@ -1,2 +1,9 @@
 # https://github.com/caarlos0/dotfiles.fish/blob/master/fzf/install.fish
-export FZF_DEFAULT_OPTS="--color=bg+:#302D41,bg:#1E1E2E,spinner:#F8BD96,hl:#F28FAD --color=fg:#D9E0EE,header:#F28FAD,info:#DDB6F2,pointer:#F8BD96 --color=marker:#F8BD96,fg+:#F2CDCD,prompt:#DDB6F2,hl+:#F28FAD"
+# export FZF_DEFAULT_OPTS="--color=bg+:#1e1e1e,bg:#000000,spinner:green,hl:#FF0000 --color=fg:#D9E0EE,header:#F28FAD,info:#DDB6F2,pointer:#F8BD96 --color=marker:#F8BD96,fg+:#F2CDCD,prompt:#DDB6F2,hl+:#F28FAD"
+
+# https://minsw.github.io/fzf-color-picker/
+export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'
+ --color=fg:#b3b3b3,bg:#000000,hl:#1f6feb
+ --color=fg+:#ffffff,bg+:#222222,hl+:#a5d6ff
+ --color=info:#484f58,prompt:#8957e5,pointer:#af5fff
+ --color=marker:#56d364,spinner:#ff9bce,header:#db61a2'
