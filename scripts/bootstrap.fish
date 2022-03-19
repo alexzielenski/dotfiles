da827f5a4a9cf66ba03d66d9ecbbca62771a115d
Alexander Zielenski <alexzielenski@gmail.com>
2022-03-18 19:26:39 -0700

    fix bug running bootstrap from within scripts dir

--- scripts/bootstrap.fish
+++ scripts/bootstrap.fish
@@ -1,5 +1,5 @@
 #!/usr/bin/env fish
-set -l DIR (dirname (status --current-filename))
+set -l DIR (realpath (dirname (status --current-filename)))
 set -x DOTFILES (realpath (dirname $DIR))
 
 ################################################################################
