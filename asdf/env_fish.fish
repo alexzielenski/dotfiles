4285bb03a52ffeb0fed2b763bdcd79835e7816c3
Alexander Zielenski <alexzielenski@gmail.com>
2022-03-20 08:27:51 -0700

    use local va

--- asdf/env_fish.fish
+++ asdf/env_fish.fish
@@ -1,7 +1,6 @@
 #!/usr/bin/env fish
 
-set asdfPath (brew --prefix asdf)"/libexec/asdf.fish"
+set -l asdfPath (brew --prefix asdf)"/libexec/asdf.fish"
 if test -f asdfPath
     source $asdfPath
 end
-set -u asdfPath
