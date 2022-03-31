a889fcb04a3f5e5c8cbfca8859e1cf9f98e33437
Alexander Zielenski <zielenski@google.com>
2022-03-31 00:06:19 -0700

    add golang to asdf

--- asdf/install.fish
+++ asdf/install.fish
@@ -9,3 +9,7 @@ asdf global python latest
 asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
 asdf install ruby latest
 asdf global ruby latest
+
+asdf plugin-add golang https://github.com/kennyp/asdf-golang.git
+asdf install golang latest
+asdf global golang latest
