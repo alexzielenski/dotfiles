f7b7e77724d0317649f900e0f26c2643c2573220
Alexander Zielenski <351783+alexzielenski@users.noreply.github.com>
2022-04-07 09:30:11 -0700

    fix typo

--- zoxide/rc_zsh.sh
+++ zoxide/rc_zsh.sh
@@ -1,4 +1,4 @@
-if command -v brew &> /dev/null
+if command -v zoxide &> /dev/null
 then
     # Must be called AFTER compinit for working completions
     # As of writing our current zsh config doesnt use completions
