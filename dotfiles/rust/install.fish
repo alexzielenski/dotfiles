582f36e42c1a98c177eb4abb8bb3f81d6095d396
Alexander Zielenski <351783+alexzielenski@users.noreply.github.com>
2022-09-03 07:31:12 -0700

    x

--- dotfiles/rust/install.fish
+++ dotfiles/rust/install.fish
@@ -1,12 +1,9 @@
 #!/usr/bin/env fish
 
-# brew install -q rust
-# brew unlink -q rust
+fish_add_path --prepend $HOME/.cargo/bin
 
 if not type -q rustup
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --no-modify-path -y
 end
 
-
-fish_add_path --prepend $HOME/.cargo/bin
 rustup update 1> /dev/null
