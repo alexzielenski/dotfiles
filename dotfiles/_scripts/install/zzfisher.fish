76c7550043333ddeab700f8d3950662ff53f8ef3
Alexander Zielenski <351783+alexzielenski@users.noreply.github.com>
2022-09-03 07:24:24 -0700

    remove link_file from fisher install

--- dotfiles/_scripts/install/zzfisher.fish
+++ dotfiles/_scripts/install/zzfisher.fish
@@ -1,8 +1,5 @@
 #!/usr/bin/env fish
 
-# Symlink fisher plugin list into dotfiles
-link_file $DOTFILES/fisher/plugins $__fish_config_dir/fish_plugins backup
-
 if not type -q fisher
     curl -sL https://git.io/fisher | source
 end
