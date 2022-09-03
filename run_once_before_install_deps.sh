53b34a954d0c0e04aa9dd5b06210457b6c234a0f
Alexander Zielenski <351783+alexzielenski@users.noreply.github.com>
2022-09-02 20:41:23 -0700

    x

--- run_once_before_install_deps.sh
+++ run_once_before_install_deps.sh
@@ -1,18 +1,40 @@
 #!/usr/bin/env bash
+echo "Installing dependencies..."
 
-# Root dependencies
+# Install Xcode command line tools on osx
+if [ "$(uname -s)" == "Darwin" ]; then
+    xcode-select --install
+fi
 
-# Install brew for Linux/OSX
 if ! command -v brew &> /dev/null
 then
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    exit
+    echo "brew could not be found"
+    echo "installing brew"
+    curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
 fi
 
-# Install Fish for Linux/OSX
 if ! command -v fish &> /dev/null
 then
-    brew install -q fish
-    exit
+    echo "fish could not be found"
+    echo "installing fish"
+    brew install fish
 fi
 
+#todo make sure fish at least 3.4.0
+currentver="$(fish -v)"
+requiredver="fish, version 3.4.0"
+if [ "$(printf '%s\n' "$requiredver" "$currentver" | sort -V | head -n1)" = "$requiredver" ]; then 
+    echo "✓ fish is at least v${requiredver}"
+else
+    echo "You must have at least fish version ${requiredver}. Please upgrade."
+    exit 1
+fi
+
+if ! command -v zsh &> /dev/null
+then
+    echo "zsh could not be found"
+    echo "installing zsh"
+    brew install zsh
+fi
+
+echo "Done installing dependencies..."
