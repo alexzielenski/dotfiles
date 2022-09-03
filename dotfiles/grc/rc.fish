d948442c92fdcdafe1110c346e16b986e7cec8cb
Alexander Zielenski <351783+alexzielenski@users.noreply.github.com>
2022-09-03 13:40:53 -0700

    add grc to path

--- dotfiles/grc/rc.fish
+++ dotfiles/grc/rc.fish
@@ -1,5 +1,5 @@
 #!/usr/bin/env fish
 
-fish_add_path --prepend --global $DOTFILES_DATA/grc/dot_grc/bin
+fish_add_path --prepend --global $DOTFILES_DATA/grc/dot_grc
 
 source $DOTFILES_DATA/grc/dot_grc/grc.fish
--- dotfiles/grc/rc.zsh
+++ dotfiles/grc/rc.zsh
@@ -1,3 +1,4 @@
 #!/usr/bin/env zsh
 
+export PATH=$DOTFILES_DATA/grc/dot_grc:$PATH
 source $DOTFILES_DATA/grc/dot_grc/grc.zsh
