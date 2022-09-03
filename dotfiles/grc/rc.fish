9ecd89d9d7fb9bf258930124b22b6376c3875bcf
Alexander Zielenski <351783+alexzielenski@users.noreply.github.com>
2022-09-03 13:20:25 -0700

    fix grc

--- dotfiles/grc/rc.fish
+++ dotfiles/grc/rc.fish
@@ -1,5 +1,5 @@
 #!/usr/bin/env fish
 
-fish_add_path --global $DOTFILES_DATA/grc/dot_grc/bin
+fish_add_path --prepend --global $DOTFILES_DATA/grc/dot_grc/bin
 
 source $DOTFILES_DATA/grc/dot_grc/grc.fish
--- dotfiles/rust/install.fish
+++ dotfiles/rust/install.fish
@@ -6,5 +6,7 @@ if not type -q rustup
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --no-modify-path -y
 end
 
+fish_add_path --prepend $HOME/.cargo/bin
+
 rustup update 1> /dev/null
     or abort "install rust"
