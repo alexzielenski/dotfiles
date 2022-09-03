4b8785aab14db076d628428c09af622dd4e662fc
Alexander Zielenski <351783+alexzielenski@users.noreply.github.com>
2022-09-03 13:01:29 -0700

    add cmake

--- dotfiles/asdf/install.fish
+++ dotfiles/asdf/install.fish
@@ -3,6 +3,10 @@
 source $DOTFILES_DATA/asdf/dot_asdf/asdf.fish
     or abort "failed to setup asdf"
 
+asdf plugin add cmake https://github.com/asdf-community/asdf-cmake.git
+asdf install cmake latest
+asdf global cmake latest
+
 asdf plugin add python https://github.com/danhper/asdf-python.git
 asdf install python latest
 asdf global python latest
@@ -11,6 +15,6 @@ asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
 asdf install ruby latest
 asdf global ruby latest
 
-asdf plugin-add golang https://github.com/kennyp/asdf-golang.git
+asdf plugin add golang https://github.com/kennyp/asdf-golang.git
 asdf install golang latest
 asdf global golang latest
