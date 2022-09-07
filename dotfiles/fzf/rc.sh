0f5df1fb3396315c7a29b61ab21dc49136f16f19
Alexander Zielenski <351783+alexzielenski@users.noreply.github.com>
2022-09-06 19:29:37 -0700

    add fzf to path

--- dotfiles/fzf/rc.sh
+++ dotfiles/fzf/rc.sh
@@ -1,3 +1,5 @@
+export PATH="$DOTFILES_DATA/fzf/dot_fzf/bin":$PATH
+
 # https://github.com/caarlos0/dotfiles.fish/blob/master/fzf/install.fish
 # export FZF_DEFAULT_OPTS="--color=bg+:#1e1e1e,bg:#000000,spinner:green,hl:#FF0000 --color=fg:#D9E0EE,header:#F28FAD,info:#DDB6F2,pointer:#F8BD96 --color=marker:#F8BD96,fg+:#F2CDCD,prompt:#DDB6F2,hl+:#F28FAD"
 
