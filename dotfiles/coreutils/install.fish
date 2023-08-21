2caadfb6fa21c0041ec60e436dbd0316fc8864a8
Alexander Zielenski <351783+alexzielenski@users.noreply.github.com>
2023-08-21 16:39:48 -0700

    install grap

--- dotfiles/coreutils/install.fish
+++ dotfiles/coreutils/install.fish
@@ -4,6 +4,7 @@ switch (uname)
 case Darwin
     brew install -q coreutils
     brew install -q gnu-sed
+    brew install -q grep
 case Linux
     # Do nothing. Should already have coreutils and sed?
 case '*'
