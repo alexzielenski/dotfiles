fc95f5f66c7eb960041d0ba92c4152ff8e6d80f0
Alex Zielenski <alexzielenski@gmail.com>
2024-08-04 06:09:54 -0700

    use <() rather than eval for style

--- dotfiles/zoxide/env_zsh.sh
+++ dotfiles/zoxide/env_zsh.sh
@@ -3,5 +3,5 @@ then
     # Must be called AFTER compinit for working completions
     # As of writing our current zsh config doesnt use completions
     export _ZO_FZF_OPTS='--scheme=path --tiebreak=end,chunk,index --bind=ctrl-z:ignore,btab:up,tab:down --cycle --keep-right --border=sharp --height=45% --info=inline --layout=reverse --tabstop=1 --exit-0 --select-1'
-    eval "$(zoxide init zsh)"
+    source <(zoxide init zsh)
 fi
