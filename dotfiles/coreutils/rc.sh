c88748fda9c2f5323f53cb4fab990eeb6da3aef2
Alex Zielenski <alexzielenski@gmail.com>
2024-08-04 05:44:16 -0700

    fix coreutils path

--- dotfiles/coreutils/rc.sh
+++ dotfiles/coreutils/rc.sh
@@ -6,9 +6,9 @@ fi
 
 # Use coreutils gnubins first in PATH to override built-in macOS versions which
 # # support non-standard flags
-export PATH="$HOMEBREW_PREFIX/grep/libexec/gnubin":$PATH
+export PATH="$HOMEBREW_PREFIX/opt/grep/libexec/gnubin":$PATH
 
 # having problems with uname -m reporting x86 on new windows
 # with zsh for some reason when we use this
 # export PATH="$(brew --prefix coreutils)/libexec/gnubin":PATH
-export MANPATH="$HOMEBREW_PREFIX/coreutils/libexec/gnuman":$MANPATH
+export MANPATH="$HOMEBREW_PREFIX/opt/coreutils/libexec/gnuman":$MANPATH
