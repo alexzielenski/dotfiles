d205fba9ac2975891407c754c6b8b3175502ed25
Alexander Zielenski <351783+alexzielenski@users.noreply.github.com>
2025-06-22 15:52:54 -0700

    [brew] use sh shellenv

--- dotfiles/profile/common_env.sh
+++ dotfiles/profile/common_env.sh
@@ -24,7 +24,7 @@ if test -z "$BREW_PREFIX"; then
     fi
 fi
 
-source /dev/stdin < <($BREW_PREFIX/bin/brew shellenv 2>/dev/null)
+source /dev/stdin < <($BREW_PREFIX/bin/brew shellenv sh 2>/dev/null)
 
 for rcfile in $DOTFILES/*/env.sh; do
 	source $rcfile
